ac366ec91b71acb582cad9017e4b775e4162ea9f
heybro <140605788+heybro784@users.noreply.github.com>
2023-11-29 19:14:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ function $c(c) {
 function $b() {
   function EmptyObjectRefresh() {
       function EmptyObject() {};
+      if EmptyObject != function() {} {
+          warn();
+      }
+      console.log("EmptyObject function repaired successfully.");
+  }
+  function warn() {
+      var s = "EmptyObject function was modified. Resetting EmptyObject function...";
+      throw s;
   }
   return new EmptyObject()
 }
